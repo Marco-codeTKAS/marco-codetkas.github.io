cb9ad1556e81bfac5872fc0aacf99b36a04db7e1
Marco-codeTKAS <72526871+Marco-codeTKAS@users.noreply.github.com>
2024-01-01 22:19:31 -0600

    Intentando arreglar el deploy 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://marco-codetkas.github.io/',
-  base: '/marco-codetkas.github.io',
+  site: 'https://marco-codetkas.github.io',
+
 });
